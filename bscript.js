35ef37c19c1181297d686257df85b1136bcf5733
marouen38 <bachtobjimarouen@gmail.com>
2019-06-27 16:51:07 +0200

    sauvegarde

---
+++ bscript.js
@@ -0,0 +1,32 @@
+function my_Function() {
+    document.getElementById("demo").innerHTML = "Bachtobji Marouen 32 ans développeur java_full_stack";
+    document.getElementById("demo3").innerHTML = "";
+    document.getElementById("demo2").innerHTML = "";
+  }
+  
+
+  function myFunction() {
+    document.getElementById("demo2").innerHTML = "2018 | Développeur"+ "<br>" +
+    "M2I GRENOBLE"+ "<br>" +
+    "Développement d’un jeu de cartes basé sur le langage JAVA SE avec l’extension JEE et le Framework SPRING en suivant l’architecture MVC."+ "<br>" +
+    "Développement des applications diverses en Java afin de bien maitriser le langage."+ "<br>" +
+    "Développer la partie front end avec Angular 6"+ "<br>" +
+   "2017 | Chef d’équipe - BTP"+ "<br>" +
+   " 2014 IFAL peinture – Grenoble"+ "<br>" +
+    "Gérer les chantiers"+ "<br>" +
+    "2014 |Professeur de Mathématique"+ "<br>" +
+    "2013 Ecole Excellent School - Bizerte"+ "<br>" +
+    "2013 | Ingénieur Hydraulique (stage)"+ "<br>" +
+   " Institut national des recherches en génie rural, eaux et forêts - Tunis."+ "<br>" +
+   " Développement des calendriers d’irrigation des cultures annuelle";
+    document.getElementById("demo").innerHTML="";
+    document.getElementById("demo3").innerHTML = "";
+
+  }
+  
+
+  function myfunction() {
+    document.getElementById("demo3").innerHTML = "11 Rue Eugène Sue 38 100 GRENOBLE \n   bachtobjimarouen@gmail.com \n  06 56 70 65 48";
+    document.getElementById("demo2").innerHTML = "";
+    document.getElementById("demo").innerHTML="";
+  }
